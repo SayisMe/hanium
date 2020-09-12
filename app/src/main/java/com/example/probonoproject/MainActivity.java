29de72cc084d8e48fc727e5e112da9b332e3e81f
sej <sejj0126@naver.com>
2020-09-13 03:28:48 +0900

    200913 블루투스 아이콘 추가

--- app/src/main/java/com/example/probonoproject/MainActivity.java
+++ app/src/main/java/com/example/probonoproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.probonoproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,8 +11,13 @@ import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    int REQUEST_ENABLE_BT = 1;
 /*
     private static final String TAG = "Main";
 
@@ -29,19 +35,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Switch btSwitch = (Switch)findViewById(R.id.bluetoothSwitch);
+
+       /* inform.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SeatInformation.class);
+                startActivity(intent);
+            }
+        });*/
+        btSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    if (!bluetoothAdapter.isEnabled()) {
+                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                    }
+                }
+                else
+                {
+                    if (bluetoothAdapter.isEnabled()) {
+                        bluetoothAdapter.disable();
+                    }
+                }
+            }
+        });
 /*
         if(btService == null) {
             btService = new BluetoothService(this, mHandler);
         }
 */
-        Button inform = findViewById(R.id.inform);
+      /*  Button inform = findViewById(R.id.inform);
         inform.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SeatInformation.class);
                 startActivity(intent);
             }
-        });
+        });*/
     }
 /*
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
